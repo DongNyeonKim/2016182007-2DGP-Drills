f61db5ff75ff0639ed3aa868dd80f1ebff3d9188
DongNyeonKim <55976867+DongNyeonKim@users.noreply.github.com>
2019-10-02 19:08:43 +0900

    Drill 04 완성
    2016182007 김동년
    https://www.youtube.com/watch?v=OED56_3iMaA

--- Drill-04/character_moves.py
+++ Drill-04/character_moves.py
@@ -3,18 +3,27 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('run_animation.png')
+character = load_image('animation_sheet.png')
 
 x=0
 frame = 0
-while (x< 800):
-    clear_canvas()
-    grass.draw(400,30)
-    character.clip_draw(frame * 100, 0, 100,100,x,90)
-    update_canvas()
-    frame = (frame+1)%8
-    x += 5
-    delay(0.05)
+while True :
+    while (x< 800):
+        clear_canvas()
+        grass.draw(400,30)
+        character.clip_draw(frame * 100, 100, 100, 100, x, 90)
+        update_canvas()
+        frame = (frame+1)%8
+        x += 1
+    while (x > 10):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+        update_canvas()
+        frame = (frame - 1) % 8
+        x -= 1
+
+    delay(0.02)
     get_events()
     
 
