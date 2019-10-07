c124d16e8414d07e153d92fca10864688d7bd0f4
DongNyeonKim <55976867+DongNyeonKim@users.noreply.github.com>
2019-10-07 18:48:10 +0900

    Drill 05
    2016182007
    2회

--- Drill-05/move_character_with_mouse.py
+++ Drill-05/move_character_with_mouse.py
@@ -13,6 +13,8 @@ def handle_events():
             running = False
         elif event.type == SDL_MOUSEMOTION:
             x,y = event.x, KPU_HEIGHT - 1 - event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            mx,my = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
