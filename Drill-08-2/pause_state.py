0b2c84e7f8f30a28531dde3ab467cf8495f530d5
DongNyeonKim <55976867+DongNyeonKim@users.noreply.github.com>
2019-10-21 18:57:18 +0900

    Drill-08-02
    2016182007 김동년
    Commit 횟수 : 1회

---
+++ Drill-08-2/pause_state.py
@@ -0,0 +1,54 @@
+import game_framework
+import main_state
+from pico2d import *
+
+
+name = "Pausestate"
+image = None
+stop = 0
+
+def enter():
+    global image
+    image = load_image('pause.png')
+
+def exit():
+    global image
+    del(image)
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
+            game_framework.pop_state()
+            #game_framework.change_state(main_state)
+
+
+def draw():
+    clear_canvas()
+    image.clip_draw(200, 200, 500, 500, 400, 300, 300, 300)
+    update_canvas()
+
+
+
+
+
+def update():
+    global stop
+    stop += 1
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+
+
+
+
