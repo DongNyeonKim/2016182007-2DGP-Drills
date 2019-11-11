01bf4c0e3a8e47dfb46c5280b85eb45625cc00e7
DongNyeonKim <55976867+DongNyeonKim@users.noreply.github.com>
2019-11-11 21:55:51 +0900

    Drill-10
    2016182007 김동년
    Commit 횟수 : 4회
    https://www.youtube.com/watch?v=5806UmDiBhg
    
    새의 크기 : 9m X 8.1m
    새의 속도 : 50km/h
    날개짓 속도 : 1

--- Drill-10/bird.py
+++ Drill-10/bird.py
@@ -2,13 +2,13 @@ import game_framework
 from pico2d import *
 
 import game_world
-PIXEL_PER_METER = (10.0/0.7)
-RUN_SPEED_KMPH = 30.0
+PIXEL_PER_METER = (10.0/0.3)
+RUN_SPEED_KMPH = 50.0
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0/60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM/60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
-TIME_PER_ACTION = 2
+TIME_PER_ACTION = 1
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 14
 
@@ -68,7 +68,7 @@ class Bird:
         if self.dir == 1:
             self.image.clip_composite_draw(self.framex * 182, self.framey * 168, 183, 168,0, '', self.x, 300,300,270)
         elif self.dir == -1:
-            self.image.clip_composite_draw(self.framex * 182, self.framey * 168, 183, 168, 3.141592 , 'v', self.x, 300, 300, 270)
+            self.image.clip_composite_draw(self.framex * 182, self.framey * 168, 183, 168, 3.141592 , 'v', self.x, 300, 300,270)
         pass
 
     def handle_event(self, event):
